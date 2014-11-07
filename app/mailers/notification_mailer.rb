bbda17115aed3672eb05a72e0d7adffd41e7855a
Brant Wellman <brantwellman@gmail.com>
2014-11-07 01:29:08 +0000

    Adjustment to the automated email mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,9 +1,11 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@brewssterapp.com"
 
-  def comment_added
-  	mail(to: "brantwellman@gmail.com",
-  		subject: "A comment has been added to your brewery")
+  def comment_added(comment)
+  	@place = comment.place
+  	@place_owner = @place.user
+  	mail(to: @place_owner.email,
+  		subject: "A comment has been added to #{@place.name}")
   end
 
 end
